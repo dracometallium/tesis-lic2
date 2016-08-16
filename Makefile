43c7d93a78689ce0ae009fc14ff589af2bd49c2c
Draco Metallium <draco.kun@gmail.com>
2016-08-16 17:00:35 -0300

    Agregando explicación pseudocódigo.

--- Makefile
+++ Makefile
@@ -11,11 +11,13 @@ DATOS=$(RES:.res=.dat)
 PRIMOS=img/primos.pdat
 AREAS=img/areas.pdat
 CACHE=img/cache.pdat
+CODIGO=img/itemSwitch.cpp
 CACHE_PDF=$(CACHE:.pdat=_fallos.pdf)
 PRIMOS_PDF=$(PRIMOS:.pdat=_fps.pdf) $(PRIMOS:.pdat=_area.pdf)
 DATOS_PDF=$(DATOS:.dat=_fps.pdf) $(DATOS:.dat=_turnArround.pdf) $(DATOS:.dat=_tFPS.pdf)
 FIGURAS_PDF=$(FIGURAS:.svg=.pdf)
-PDF=$(FIGURAS_PDF) $(DATOS_PDF) $(PRIMOS_PDF) $(CACHE_PDF)
+CODIGO_PDF=$(CODIGO:.cpp=.pdf)
+PDF=$(FIGURAS_PDF) $(DATOS_PDF) $(PRIMOS_PDF) $(CACHE_PDF) $(CODIGO_PDF)
 GARBAGE=*.aux *.bbl *.blg *.log *.pdf *.toc
 
 all: $(NAME).pdf
@@ -46,6 +48,17 @@ $(PRIMOS_PDF): $(PRIMOS) $(AREAS)
 
 $(CACHE_PDF): $(CACHE)
 	gnuplot ./scripts/cache.gnuplot
+
+
+$(CODIGO_PDF): $(CODIGO)
+	echo ":set syntax \n\
+	:set number \n\
+	:set printfont=currier:8 \n\
+	:set printoptions=number:y,header:0 \n\
+	:colorscheme default \n\
+	:hardcopy > "$(<:.cpp=.ps) "\n:q\n" | vim $<
+	inkscape $(<:.cpp=.ps) -z --export-area-drawing -A $@
+	rm $(<:.cpp=.ps)
 
 clean:
 	rm -f $(GARBAGE)
---
+++ img/itemSwitch.cpp
@@ -0,0 +1,71 @@
+int main(){
+    //Código de inicialización
+    continuar=true;
+#pragma omp parallel sections num_threads(3)
+    {
+#pragma omp section
+        {
+            generaciónDeCuadros(fuente, pilaCuadros, &continuar);
+        }
+#pragma omp section
+        {
+           generaciónDeTareasDeFragmentaciónDeCuadros
+                (NHilos, NPartes, pilaCuadros, &continuar);
+        }
+#pragma omp section
+        {
+                esperarSeñalFin();
+                continuar=false;
+        }
+    }
+    //Código de finalización
+    return 0;
+}
+
+int generaciónDeTareasDeFragmentaciónDeCuadros
+        (NHilos, NPartes, pilaCuadros, continuar)
+{
+    hilosLibre = NHilos;
+#pragma omp parallel num_threads(NHilos + 1) default(shared)
+#pragma omp single
+    while(*continuar) {
+        cuadro = NULL;
+        if ((pilaCuadros->cantidad() > 0) && (hilosLibres > 0)) {
+#pragma omp critical (RingStack)
+            {
+                cuadro = pilaCuadros->siguiente();
+            }
+        }
+        if (cuadro != NULL) {
+#pragma omp atomic
+            hilosLibres--;
+#pragma omp task firstprivate(cuadro) default(shared)
+            if (*continuar) {
+                deltaHilos = NHilos - 1;
+                p = fraccionar(cuadro, NPartes);
+#pragma omp atomic
+                hilosLibres = hilosLibres - deltaHilos;
+                for (t = 0; t < NPartes; t++)
+#pragma omp task firstprivate(t) default(shared)
+                {
+                    for (i = 0; i < pilas->cantidad(); i++) {
+                        RestaurarCuadro(p[t]);
+                        pilas[i]->procesar(p[t]);
+                    }
+                    borrarParte(p[t]);
+#pragma omp atomic
+                    hilosLibres++;
+                }
+
+#pragma omp taskwait
+#pragma omp atomic
+                hilosLibres--;
+                delete Cuadro;
+#pragma omp atomic
+                hilosLibres++;
+            }
+        }
+    }
+#pragma omp taskwait
+    return 0;
+}
