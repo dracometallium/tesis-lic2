1b286f8dc5ad3961ab9771687efcb25d450654cc
Draco Metallium <draco.kun@gmail.com>
2016-11-18 15:07:06 -0300

    Removiendo tabs viejos

--- scripts/mdat.sh
+++ scripts/mdat.sh
@@ -22,43 +22,43 @@ awkProg='
 
 bkIFS=$IFS
 arch=$1
-	dir=$(dirname $1)
-	base=$(basename -s .res $arch)
-	rFile=${dir}/${base}.dat
-	echo -n > $rFile
-	tFile=$(mktemp)
-	for t in $(seq 1 12); do
-		for p in $(seq 1 24); do
-			echo -n > $tFile
-			grep $arch -e "^$t $p " > $tFile
-			fps=$(
-				cut -f 4 -d ' ' $tFile |
-				sort -n |
-				sed -e "1q"
+dir=$(dirname $1)
+base=$(basename -s .res $arch)
+rFile=${dir}/${base}.dat
+echo -n > $rFile
+tFile=$(mktemp)
+for t in $(seq 1 12); do
+	for p in $(seq 1 24); do
+		echo -n > $tFile
+		grep $arch -e "^$t $p " > $tFile
+		fps=$(
+			cut -f 4 -d ' ' $tFile |
+			sort -n |
+			sed -e "1q"
+		)
+		mWait=$(
+			cut -f 5 -d ' ' $tFile |
+			sort -nr |
+			sed -e "1q"
 			)
-			mWait=$(
-				cut -f 5 -d ' ' $tFile |
-				sort -nr |
-				sed -e "1q"
-				)
-			err=$(
-				awk "$awkProg" $tFile
-				)
-			grep ${dir}/${base}.bres -e "^$t $p " > $tFile
-			mbWait=$(
-				cut -f 5 -d ' ' $tFile |
-				sort -nr |
-				sed -e "1q"
-				)
-			berr=$(
-				awk "$awkProg" $tFile
-				)
-			echo $t $p $fps $mWait $mbWait $err $berr >> $rFile
-		done
+		err=$(
+			awk "$awkProg" $tFile
+			)
+		grep ${dir}/${base}.bres -e "^$t $p " > $tFile
+		mbWait=$(
+			cut -f 5 -d ' ' $tFile |
+			sort -nr |
+			sed -e "1q"
+			)
+		berr=$(
+			awk "$awkProg" $tFile
+			)
+		echo $t $p $fps $mWait $mbWait $err $berr >> $rFile
 	done
-	rm $tFile
-	IFS=$(printf "\n\t")
-	for j in $(cat $rFile | cut -f 3 -d ' '| sort | uniq); do
-		cat $rFile | cut -f 3,4 -d ' ' | grep "^$j " | sort -k 2nr | sed '1q';
-	done > ${dir}/${base}.tdat
-	IFS=$bkIFS
+done
+rm $tFile
+IFS=$(printf "\n\t")
+for j in $(cat $rFile | cut -f 3 -d ' '| sort | uniq); do
+	cat $rFile | cut -f 3,4 -d ' ' | grep "^$j " | sort -k 2nr | sed '1q';
+done > ${dir}/${base}.tdat
+IFS=$bkIFS
--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-	arch=$1
-	dir=$(dirname $1)
-	resolution=$(basename -s .dat $arch)
-	gnuplot -e "arch='"$arch"';resolution='"$resolution"';dir='"$dir/"';" scripts/plot.gnuplot
+arch=$1
+dir=$(dirname $1)
+resolution=$(basename -s .dat $arch)
+gnuplot -e "arch='"$arch"';resolution='"$resolution"';dir='"$dir/"';" scripts/plot.gnuplot
